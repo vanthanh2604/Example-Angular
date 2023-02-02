7ed0d29d3504f8c9c7a4cdb22daaa0fc02b52a3f
vanthanh2604 <67373376+vanthanh2604@users.noreply.github.com>
2023-02-02 14:28:46 +0700

    1 binaries uploaded

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsResolverService } from './services/productsResolver/products-resolver.service';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'productsccccccc',
+    component: ProductsComponent,
+    resolve: { productsccccccc: ProductsResolverService }
+  },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
